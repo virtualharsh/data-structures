f710c91c28c8ec5b2dea42a98361d22ac1457185
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-03-15 09:38:18 +0530

    fixed prime number on input 1

--- loops/Driver2.java
+++ loops/Driver2.java
@@ -3,6 +3,9 @@
 
 class Prime{
     public static boolean isPrime(int n){
+        if(n==1){
+            return false;
+        }
         int l = (int) java.lang.Math.ceil(java.lang.Math.sqrt(n));
         for(int i=2 ; i<l ; i++){
             if(n%i==0){
@@ -15,6 +18,7 @@ class Prime{
 
 public class Driver2 {
     public static void main(String[] args) {
-        System.out.println(Prime.isPrime(20));
+        System.out.println(Prime.isPrime(3));
+        System.out.println(Prime.isPrime(1));
     }
 }
