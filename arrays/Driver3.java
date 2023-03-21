477b32c1fdf7c78fa93b6004c423508495118989
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-03-21 08:43:15 +0530

    added array problems

---
+++ arrays/Driver3.java
@@ -0,0 +1,34 @@
+// 3.	Find unique value in odd size array. Every other element is present twice in array only a single value there with 1 occurrence. Return that value.
+import java.util.HashSet;
+
+class ArrayFindUnique {
+    public static int findUnique(Integer[] a) {
+        int unique=0;
+        for(int i=0 ; i<a.length ;i++){
+            unique = unique ^ a[i];
+        }
+        return unique;
+    }
+    
+    public static int findUniqueWithHashSet(Integer[] a) {
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for (int i = 0; i < a.length; i++) {
+            if(hs.contains(a[i])){
+                hs.remove(a[i]);
+            }else{
+                hs.add(a[i]);
+            }
+        }
+        Integer temp[] = new Integer[hs.size()];
+        hs.toArray(temp);
+        return temp[0];
+    }
+}
+
+public class Driver3 {
+    public static void main(String[] args) {
+        Integer a[] = {10,20,25,20,10};
+        System.out.print(ArrayFindUnique.findUniqueWithHashSet(a));
+    }
+}
---
+++ arrays/Driver4.java
@@ -0,0 +1,6 @@
+class ArrayUniqueOccurances{
+    
+}
+public class Driver4 {
+    
+}
