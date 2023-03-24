0a69d027edf45e6443a11cc1e27642977adb2aa7
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-03-24 20:18:45 +0530

    added tripet sum solution

--- arrays/Driver9.java
+++ arrays/Driver9.java
@@ -4,6 +4,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class ArrayPairSum {
+    public static Integer[][] findPairSum(Integer a[],Integer start, Integer target) {
+        ArrayList<Integer[]> ans = new ArrayList<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = start; i < a.length; i++) {
+            if (hm.containsKey(target - a[i])) {
+                Integer temp[] = { hm.get(target - a[i]), i };
+                ans.add(temp);
+            }
+            hm.put(a[i], i);
+        }
+        Integer temp[][] = new Integer[ans.size()][2];
+        ans.toArray(temp);
+        return temp;
+    }
+
+    public static ArrayList<Integer[]> findTriplet(Integer a[],Integer target){
+        ArrayList<Integer[]> ans = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            Integer[][] pairSumAns = findPairSum(a,i+1,target-a[i]);
+            if(pairSumAns.length!=0){
+                for (Integer[] j : pairSumAns) {
+                    Integer toAdd[] = {i,j[0], j[1]};
+                    ans.add(toAdd);
+                }
+            }
+        }
+        return ans;
+    } // o(n*n)
 
     public static void displayAnswer(ArrayList<Integer[]> ans) {
         System.out.print("[");
@@ -17,7 +46,9 @@ class ArrayPairSum {
 
 public class Driver9 {
     public static void main(String[] args) {
-        Integer a[] = { 10, 20, 30, 40, 50, 60 , 70 , 80};
+        Integer a[] = { 80,10,20,30,40,50,70,60};
         int target = 110;
+        ArrayList<Integer[]> ans = ArrayPairSum.findTriplet(a, target);
+        ArrayPairSum.displayAnswer(ans);
     }
 }
