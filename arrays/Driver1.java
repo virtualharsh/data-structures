f7e9445e8cce788c73c994f29d0ba2fe2ff8a17e
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-03-28 07:38:16 +0530

    modified

--- arrays/Driver1.java
+++ arrays/Driver1.java
@@ -1,63 +1,75 @@
-// 1.	implement all common array operations that includes traversal like finding maximum,minimum, sum , product ,linear search , reverse array.
+// 1.	Implement all common array operations that includes traversal like finding maximum, minimum, sum, product, linear search, reverse array, insert/delete at last, insert/delete at specific location.(do all this solution in single class and try to reuse the loop for function).
 
 class ArrayOperation{
-    private int max , min,sum;
+    public Integer a[];
+    private int max , min ,sum = 0, startPointer,endPointer, product = 1;
 
-    public void display(int a[]){
-        for(int i=0 ; i<a.length ; i++){
-            System.out.print(a[i] + " ");
-        }
-        System.out.println();
+    ArrayOperation(int size){
+        a = new Integer[size];
+        startPointer = -1;
+        endPointer= -1;
     }
-    public int getMax(int a[]) {
+    
+    private void changeProperties(){
+        if(endPointer==-1){
+            return ;
+        }
         max = a[0];
         min = a[0];
         sum = 0;
-
-        for(int i=1 ; i<a.length ; i++){
-            sum+=a[i];
-            if(a[i]>max)
+        product =1;
+        for(int i=startPointer ; i<endPointer ; i++){
+            if(a[i]>max){
                 max = a[i];
-            if(a[i]<min)
+            }
+            if(a[i]<min){
                 min = a[i];
+            }
+            sum += a[i];
+            product *= a[i];
         }
+    }
+
+    private void resetProperties(){
+        max = 0;
+        min =0;
+        product=1;
+        sum=0;
+    }
+
+    public void display(){
+        System.out.println( "max:" + max  + " min:" + min + " sum:" + sum + " product:" + product);
+        int i = startPointer;
+        while(i<endPointer){
+            System.out.print(a[i] + " ");
+            i++;
+        }
+        System.out.println();
+    }
+
+    public int getMax(){
         return max;
     }
-    
-    public int getMin(int a[]) {
-        this.getMax(a);
+
+    public int getMin(){
         return min;
     }
 
-    public int sum(int[] a){
-        this.getMax(a);
+    public int getSum(){
         return sum;
     }
 
-    public boolean isAvaliable(int a[],int target) {
-        for(int i=0;i<a.length ; i++){
-            if(a[i]==target){
-                return true;
-            }
+    public int getProduct(){
+        if(startPointer==-1){
+            return 0;
         }
-        return false;
-    }
-    public void reverse(int a[]){
-        for(int i=a.length-1 ; i>=0 ; i--)
-            System.out.print(a[i] + " ");
-        System.out.println();
+        return product;
     }
 }
 
 class Driver1{
     public static void main(String[] args) {
-        int a[] = {10,20,15};
-        ArrayOperation ob = new ArrayOperation();
-        ob.display(a);
-        ob.display(a);
-        System.out.println("Minimum is " + ob.getMin(a) + "\nMaximum is " + ob.getMax(a));
-        System.out.println("sum is " + ob.sum(a));
-        System.out.println("Is 20 present " + ob.isAvaliable(a, 20));
-        ob.reverse(a);
+        ArrayOperation ob = new ArrayOperation(7);
+        
     }
 }
