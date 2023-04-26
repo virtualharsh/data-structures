ef9094ea5da46323c8a99987da2df3950225a4bf
Harsh Sonegra <imharshsonegra9577@gmail.com>
2023-04-26 08:09:20 +0530

    added rotate array

---
+++ arrays/Driver15.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Array{
+    public static int findKthLargest(Integer a[],int k){
+        if(k>a.length || k<0)
+            return -1;
+        Arrays.sort(a);
+        return a[a.length - k];
+    }   
+}   
+
+public class Driver15 {
+    public static void main(String[] args) {
+        Integer a[] = { 10,20,30,40,50 };
+        System.out.println(Array.findKthLargest(a, 2));
+    }
+}
